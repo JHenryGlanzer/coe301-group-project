726c71c2b58a3676313e1eed5bad0852ec5bc42c
JoshRichmond06 <jhr2696@eid.utexas.edu>
2026-04-05 12:34:19 -0500

    Finished Main file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
-#include "TreasureHunt.h"
 #include <iostream>
+#include "TreasureHunt.h"
 
-int main() {
-    TreasureHunt game;
-    game.startGame();
+int main()
+{
+    TreasureHunt game;  //loading in treasure hunt game class
+
+    if (!game.loadMap("map.txt"))   //calling function and making sure it works
+    {
+        std::cout << "Error loading map.txt\n";
+        return 1;
+    }
+
+    if (!game.loadClues("clues.txt"))   //calling function and making sure it works
+    {
+        std::cout << "Error loading clues.txt\n";
+        return 1;
+    }
+
+    game.startGame();   //starting the game loop
+    return 0;
 }
