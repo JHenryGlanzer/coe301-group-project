96d512e0a487ac2ebbf209983389ba837d41ac71
refeg16 <refegwork@gmail.com>
2026-04-05 14:52:04 -0500

    Clue.h
    Done

--- Clue.h
+++ Clue.h
@@ -1,1 +1,31 @@
+#ifndef CLUE_H
+#define CLUE_H
 
+#include <string>
+using namespace std;
+
+class Clue {
+private:
+    char symbol;
+    string question;
+    string answer;
+    int maxAttempts;
+    int points;
+    bool completed;
+
+public:
+    Clue();
+    Clue(char s, string q, string a, int attempts, int pts);
+
+    char getSymbol();
+    string getQuestion();
+    int getMaxAttempts();
+    int getPoints();
+    bool isCompleted();
+
+    void setCompleted(bool status);
+
+    bool checkAnswer(string userAnswer);
+};
+
+#endif
